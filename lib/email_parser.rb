d269c41208aa3f272985f47a44b4a12b34a30b5a
John Tuttle <thejwt@gmail.com>
2017-09-28 22:16:34 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailParser
   end
 
   def parse
-    @emails = @emails.scan(/[a-zA-Z0-9_@.-]+/).delete_if {|email| @emails.scan(/[a-zA-Z0-9_@.-]+/).include?(email)}
+    @emails = @emails.scan(/[a-zA-Z0-9_@.-]+/)
     @emails
   end
 
